e4492c991c4d4489395850fe0ac8e13209fe1a16
Sinan Demiroz <ton@email.com>
2025-07-22 16:09:18 +0200

    Mise en place des URL

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', function () {
+    return 'Liste des produits !';
+});
+
+Route::get('/product/{id}', function (string $id) {
+    return "product $id"; // où return 'product '.$id;
+});
+
+Route::get('/cart', function () {
+    return 'Panier';
+});
